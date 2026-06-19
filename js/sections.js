0526981cc7138ce4b4e708cbd016e77a8c023679
QuarkInComputing <jackmcleanwilson@gmail.com>
2026-06-19 21:39:12 +0100

    Create sections

--- js/sections.js
+++ js/sections.js
@@ -1,1 +1,9 @@
 const links = document.getElementById('nav-links').children;
+const sections = document.getElementsByTagName('section');
+
+function showSection(selected){
+    Array.from(sections).forEach(sec => {
+        sec.classList.remove('visible-section');
+    });
+    document.getElementById(`section-${selected}`).classList.add('visible-section');
+}
