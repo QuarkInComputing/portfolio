34ee6f037bbd5c394aaaf38f5b78c2ad0c9cda4a
Jack <jackmcleanwilson@gmail.com>
2026-08-01 16:48:30 +0100

    Added view images & desc

--- data/projectData.js
+++ data/projectData.js
@@ -1,6 +1,15 @@
 export const projectData = [
     {
         id: "c4c", 
-        title: "\"Cascade4Change\" Charity Website"
+        title: "\"Cascade4Change\" Charity Website",
+        image: "Cascade4Change.webp",
+        desc: "I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works."
+    },
+    {
+        id: "apcc",
+        title: "Ardersier & Petty Community Council",
+        image: "APCC.webp",
+        desc: "ARDERSIER, SAUL LIVES HERE!"
     }
+    
 ];
--- js/view.js
+++ js/view.js
@@ -1,16 +1,25 @@
 import { projectData } from "../data/projectData.js";
 
 const projectTitle = document.getElementById('view-title');
+const projectImage = document.getElementById('view-image');
+const projectDesc = document.getElementById('view-desc');
+
+console.log(projectData);
 
 function viewProject(project) {
 
     console.log(project);
     console.log(projectData);
 
+    var foundProject = false;
     projectData.forEach(element => {
-        if(element.id = project) {
+        if(element.id == project && !foundProject) {
             projectTitle.innerHTML = element.title;
+            projectImage.src = `../data/work_images/${element.image}`;
+            projectDesc.innerHTML = element.desc;
+            foundProject = true;
         }
+        console.log(projectData);
     });
 };
 
