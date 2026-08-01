8f2ecb37d0cb9589af84ec1afed81c3f26c5cbaf
Jack <jackmcleanwilson@gmail.com>
2026-08-01 17:03:31 +0100

    View page animation

--- data/projectData.js
+++ data/projectData.js
@@ -3,7 +3,7 @@ export const projectData = [
         id: "c4c", 
         title: "\"Cascade4Change\" Charity Website",
         image: "Cascade4Change.webp",
-        desc: "I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works. I will yap about the project here, but suffice to say: this works."
+        desc: "This website, still currently under development, is being made for a charity working for better standards in care homes following a local scandal that shocked the relatives of the residents. The design I created and implemented focuses on a sleek and easy to use interface that at all points directs the user to donate first and foremost. The chosen colour scheme works to direct the user through the websites pages, creating a call-to-action to all important elements. The website also features a blog system that works entirely through the storing of JSON files, elimating the need for a database - saving the charity money as less money needs to be spent on compute."
     },
     {
         id: "apcc",
